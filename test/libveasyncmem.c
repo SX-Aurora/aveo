f899dc35919c652796bce3ad7b273ca385204269
Erich Focht <efocht@gmail.com>
2020-05-14 19:23:12 +0200

    Adding test_memtransfers (from N. Weber) which tests odd transfer sizes. Tweaked a bit the async memory copy test.

--- test/libveasyncmem.c
+++ test/libveasyncmem.c
@@ -5,7 +5,7 @@ int64_t buffer = 0xdeadbeefdeadbeef;
 
 uint64_t print_buffer()
 {
-  printf("0x%016lx\n", buffer);
+  printf("on VE: 0x%016lx\n", buffer);
   fflush(stdout);
   return 1;
 }
--- test/test_async_mem.c
+++ test/test_async_mem.c
@@ -13,10 +13,10 @@ main()
     exit(1);
   }
   uint64_t handle = veo_load_library(proc, "./libveasyncmem.so");
-  printf("handle = %p\n", (void *)handle);
+  //printf("handle = %p\n", (void *)handle);
 
   struct veo_thr_ctxt *ctx = veo_context_open(proc);
-  printf("VEO context = %p\n", ctx);
+  //printf("VEO context = %p\n", ctx);
   struct veo_args *argp = veo_args_alloc();
 
   uint64_t buffer = 0;
@@ -25,28 +25,39 @@ main()
 
   uint64_t req;
   req = veo_async_read_mem(ctx, &buffer, bufptr, sizeof(buffer));
-  printf("veo_async_read_mem() returned %ld\n", req);
+  //printf("veo_async_read_mem() returned %ld\n", req);
   veo_call_wait_result(ctx, req, &retval);
 
   printf("%016lx\n", buffer);
+  if (buffer != 0xdeadbeefdeadbeef) {
+    printf("Unexpected buffer content!\n");
+    return 1;
+  }
 
   uint64_t id = veo_call_async(ctx, sym, argp);
-  printf("veo_call_async() returned %ld\n", id);
+  //printf("veo_call_async() returned %ld\n", id);
   ret = veo_call_wait_result(ctx, id, &retval);
-  printf("0x%lx: %d, %lu\n", id, ret, retval);
+  //printf("0x%lx: %d, %lu\n", id, ret, retval);
+  if (ret) {
+    printf("Failed to execute VE kernel function. rc=%d\n", ret);
+    return 2;
+  }
 
-  buffer = 0xc0ffee;
-  req = veo_async_write_mem(ctx, bufptr, &buffer, sizeof(buffer));
-  printf("veo_async_write_mem() returned %ld\n", req);
-
-  id = veo_call_async(ctx, sym, argp);
-  printf("veo_call_async() returned %ld\n", id);
-  ret = veo_call_wait_result(ctx, id, &retval);
-  printf("0x%lx: %d, %lu\n", id, ret, retval);
+  printf("Writing from/to unaligned address...\n");
+  buffer = 0x0403020100c0ffee;
+  req = veo_async_write_mem(ctx, bufptr + 1, (char *)(&buffer) + 1, 5);
   ret = veo_call_wait_result(ctx, req, &retval);
-  printf("0x%lx: %d, %lu\n", req, ret, retval);
+
+  req = veo_async_read_mem(ctx, &buffer, bufptr, sizeof(buffer));
+  veo_call_wait_result(ctx, req, &retval);
+
+  printf("unaligned write/read of 5 bytes: %016lx\n", buffer);
+  if (buffer != 0xdead020100c0ffef) {
+    printf("Unexpected buffer content!\n");
+    return 3;
+  }
 
   int close_status = veo_context_close(ctx);
-  printf("close status = %d\n", close_status);
+  //printf("close status = %d\n", close_status);
   return 0;
 }
