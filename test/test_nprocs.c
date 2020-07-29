746ad2fed68fb04f29a3c84feea52ecd77e2161f
veos-sxarr <veos@sxarr.jp.nec.com>
2020-07-29 16:38:49 +0900

    Updated test_nprocs to test multiple processes even if one VE exist

--- test/test_nprocs.c
+++ test/test_nprocs.c
@@ -1,3 +1,5 @@
+#define _DEFAULT_SOURCE /* for setenv */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -10,17 +12,22 @@ int main(int argc, char *argv[])
 {
   int err = 0, rc = 0;
   const int nprocs = 4;
+  int created_procs = 0;
 
   struct veo_proc_handle *proc[nprocs];
   uint64_t libh[nprocs], sym[nprocs];
 
+  setenv("OMP_NUM_THREADS", "4", 1);
+
+  for (int i = 0; i < nprocs; i++) {
+    proc[i] = NULL;
+  }
   for (int i = 0; i < nprocs; i++) {
     proc[i] = veo_proc_create(i % 2);
     printf("proc%d = %p\n", i, (void *)proc[i]);
-    if (proc[i] == NULL) {
-      rc = -1;
-      goto done;
-    }
+    if (proc[i] == NULL)
+      continue;
+    created_procs++;
 
     libh[i] = veo_load_library(proc[i], "./libvehello.so");
     printf("libh%d = %p\n", i, (void *)libh[i]);
@@ -39,6 +46,8 @@ int main(int argc, char *argv[])
 
   struct veo_args *argp = veo_args_alloc();
   for (int i = 0; i < nprocs; i++) {
+    if (proc[i] == NULL)
+      continue;
     uint64_t result = 0;
     veo_args_clear(argp);
     veo_args_set_i32(argp, 0, 42);
@@ -49,6 +58,10 @@ int main(int argc, char *argv[])
   veo_args_free(argp);
 
  done:
+  if (created_procs <= 1) {
+    printf("created_procs is %d\n", created_procs);
+    rc = -1;
+  }
   for (int i = 0; i < nprocs; i++) {
     if (proc[i] != NULL) {
       err = veo_proc_destroy(proc[i]);
