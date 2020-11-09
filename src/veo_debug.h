540a2fb8eedadbde9f049df32d58920f6af3e4a1
Erich Focht <efocht@gmail.com>
2020-11-09 23:13:21 +0100

    Add thread ID info to VEO_TRACE and VEO_DEBUG messages.

--- src/veo_debug.h
+++ src/veo_debug.h
@@ -4,33 +4,45 @@
 //#define DEBUGMEM
 //#define SYNCDMA
 
+#ifndef __ve__
+#include <unistd.h>
+#include <sys/syscall.h>
+#ifndef __cplusplus
+long syscall(long n, ...);
+#endif
+static inline int _gettid(void)
+{
+  return syscall(SYS_gettid);
+}
+#endif
+
 extern int veo_debug;
 
 #ifdef __ve__
-#define veo_dprintf(fmt, ...) do {                \
-    if (veo_debug == 1) { \
-        fprintf(stdout, "[VE] " fmt, ## __VA_ARGS__);    \
-        fflush(stdout); \
-    }                              \
+#define veo_dprintf(fmt, ...) do {                                      \
+    if (veo_debug == 1) {                                               \
+      fprintf(stdout, "[VE] " fmt, ## __VA_ARGS__);  \
+      fflush(stdout);                                                   \
+    }                                                                   \
   } while(0)
 #else
-#define veo_dprintf(fmt, ...) do {                                       \
-    if (veo_debug == 1) { \
-        fprintf(stdout, "[VH] " fmt, ## __VA_ARGS__);         \
-        fflush(stdout);                             \
-    } \
+#define veo_dprintf(fmt, ...) do {                                      \
+    if (veo_debug == 1) {                                               \
+      fprintf(stdout, "[VH] [TID %d]" fmt, _gettid(), ## __VA_ARGS__);    \
+      fflush(stdout);                                                   \
+    }                                                                   \
   } while(0)
 #endif
 
 #ifdef __ve__
-#define veo_eprintf(fmt, ...) do {               \
-    fprintf(stdout, "[VE] ERROR: " fmt, ## __VA_ARGS__);        \
-    fflush(stdout);                             \
+#define veo_eprintf(fmt, ...) do {                                      \
+    fprintf(stdout, "[VE] ERROR: " fmt, ## __VA_ARGS__); \
+    fflush(stdout);                                                     \
   } while(0)
 #else
-#define veo_eprintf(fmt, ...) do {                                        \
-    fprintf(stdout, "[VH] ERROR: " fmt, ## __VA_ARGS__);              \
-    fflush(stdout);                             \
+#define veo_eprintf(fmt, ...) do {                                      \
+    fprintf(stdout, "[VH] [TID %d] ERROR: " fmt, _gettid(), ## __VA_ARGS__); \
+    fflush(stdout);                                                     \
   } while(0)
 #endif
 
