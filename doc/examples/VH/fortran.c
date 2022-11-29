389832c1eb54a0a4d06f44f0ea2cc8c776d3a0b4
veos-sxarr <veos@sxarr.jp.nec.com>
2022-11-29 20:24:19 +0900

    Updated doxygen to add descriptions and restrictions of performance information file name. Added sections to the getting starte Fixed example code

--- doc/examples/VH/fortran.c
+++ doc/examples/VH/fortran.c
@@ -1,19 +1,20 @@
 #include <ve_offload.h>
+#include <stdio.h>
 int main()
 {
   /* Load "vefortran" on VE node 0 */
   struct veo_proc_handle *proc = veo_proc_create_static(0, "./vefortran");
-  uint64_t handle = NULL;/* find a function in the executable */
+  uint64_t handle = 0UL;/* find a function in the executable */
   struct veo_thr_ctxt *ctx = veo_context_open(proc);
   struct veo_args *argp = veo_args_alloc();
   long x = 42;
   long y;
-  veo_args_set_stack(argp, VEO_INTENT_IN, 0, &x, sizeof(x));
-  veo_args_set_stack(argp, VEO_INTENT_OUT, 1, &y, sizeof(y));
+  veo_args_set_stack(argp, VEO_INTENT_IN, 0, (char *)&x, sizeof(x));
+  veo_args_set_stack(argp, VEO_INTENT_OUT, 1, (char *)&y, sizeof(y));
   uint64_t id = veo_call_async_by_name(ctx, handle, "sub1_", argp);
   uint64_t retval;
   veo_call_wait_result(ctx, id, &retval);
-  printf("SUB1 return %lu\n", retval);
+  printf("SUB1 outputs %lu\n", y);
   veo_args_clear(argp);
   veo_args_set_i64(argp, 0, 1);
   veo_args_set_i64(argp, 1, 2);
--- doc/examples/VH/hello_static.c
+++ doc/examples/VH/hello_static.c
@@ -3,8 +3,10 @@ int main()
 {
   /* Load "vehello" on VE node 0 */
   struct veo_proc_handle *proc = veo_proc_create_static(0, "./vehello");
-  uint64_t handle = NULL;/* find a function in the executable */
+  uint64_t handle = 0UL;/* find a function in the executable */
+
   struct veo_thr_ctxt *ctx = veo_context_open(proc);
+
   struct veo_args *argp = veo_args_alloc();
   uint64_t id = veo_call_async_by_name(ctx, handle, "hello", argp);
   uint64_t retval;
